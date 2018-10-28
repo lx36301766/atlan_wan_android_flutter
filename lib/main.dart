ddaa2033cee594bfd072f59104942e2321b87059
lx364301766@gmail.com <lx364301766@gmail.com>
2018-10-29 00:29:50 +0800

    change splash

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,16 @@ import 'package:atlan_wan_android_flutter/page/splash.dart';
 
 void main() => runApp(WanAndroidApp());
 
+class WanAndroidApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Atlan WanAndroid',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: SplashPage(),
+    );
+  }
+}
