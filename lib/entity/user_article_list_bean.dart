cf6c09f9a6ecb32f4265ae1d5eff04aea20b02f5
lx364301766@gmail.com <lx364301766@gmail.com>
2020-05-24 22:45:51 +0800

    add square list
    (cherry picked from commit b161a462ae1cc3d23696d5d57dc82ece434ac5fd)
    
    # Conflicts:
    #	lib/network/api.dart
    #	lib/network/api_url.dart
    #	lib/page/general_article_list_page.dart
    #	lib/widget/article_item_widget.dart

---
+++ lib/entity/user_article_list_bean.dart
@@ -0,0 +1,10 @@
+
+import 'package:atlan_wan_android_flutter/util/unescape_json_convert.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+@UnescapeJsonConvert()
+class UserArticleListBean {
+
+}
+
